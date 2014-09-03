4bf589ce0e8aa4d0a201f949fb6ef429d4442d5c
bjarniolsen <bjo@dis-play.dk>
2014-09-03 14:27:11 +0200

    Book is done
    But could use some fanzy loader and sone keyboard controls

--- static/js/dist/build.js
+++ static/js/dist/build.js
@@ -18,58 +18,89 @@ jQuery.noConflict();
 jQuery(function ($) {
     Engine = {
         init: function () {
+            // we keep all information in this object
             var options = {};
-        	// all pages in this folder
-        	options._PAGEDIR_ = "/sider/";
-            // Load frontpage on first init
-            // unless cookie has page information
-        	options.url = "side0";
-            options.pageNumber = 0;
+        	options.startPage = "side0";
+        	// pages count = number of images / 2
+			options.numberOfPages = 60;
             // Cache some dom
-            options.root = $(".site-wrapper");
-            options.menu = options.root.find(".primary-nav");
+            options.content = $(".site-content");
+            options.menu = options.content.find(".primary-nav");
             options.menuItems = options.menu.find(".primary-nav__link");
-            options.content = options.root.find(".site-content");
+            //options.content = options.root.find(".site-content");
 			// we need to set a height on the page wrapper
-			options.content.css("height", $(".dummy img").height());
-            options.paginator = options.root.find(".js-paginator");
+            options.paginator = options.content.find(".js-paginator");
             options.paginator.next = options.paginator.find(".js-paginator__next");
             options.paginator.prev = options.paginator.find(".js-paginator__prev");
+
+			// build pages
+			var index = 0;
+			while (index < options.numberOfPages) {
+				pageId = "side" + index;
+				imageId = index;
+				// create new dom element and add new html to it
+				var cache = Engine.ui.addPageToDom(pageId, imageId);
+				// and add it to our wrapper
+				options.content.prepend(cache);
+				// clear it for next use
+				cache = "";
+				index++;
+
+				if (index === options.numberOfPages) {
+            		// Load frontpage on first init
+            		// unless cookie has page information
+        			options.url = options.startPage;
+        			options.previousPage = options.url;
+					$("#side0").addClass("is-active");
+
+					window.setTimeout(function() {
+						options.content.css("height", $("#side0").find("img").height());
+					}, 1300);
+				}
+			}
+			
             // Custom event
             $(document).bind("load.page", function(event, options) {
-        		if ($('#' + options.url).length) {
-					Engine.ui.showPage(options);
-				} else {
-					Engine.ui.getPage(options);
+				console.log(options.url);
+				if (!(options.pageNumber <= options.numberOfPages - 1)) {
+					options.pageNumber = 0;
+        			options.url = options.url.replace(/\d+/g, options.pageNumber);
 				}
+				Engine.ui.showPage(options);
             });
-            options.menu.on("click", function() {
-            	if($(this).hasClass("is-active")) {
-            		$(this).removeClass("is-active");
-            	} else {
-            		$(this).addClass("is-active");
+            options.menu.on({
+            	click: function() {
+            		if($(this).hasClass("is-active")) {
+            			$(this).removeClass("is-active");
+            		} else {
+            			$(this).addClass("is-active");
+            		}
+            	},
+            	mouseout: function() {
+					//options.menu.removeClass("is-active");
+					//console.log($(this));
             	}
             });
 			// Click events
 			options.paginator.next.on("click", function() {
-				options.pageturner = "next";
         		// get pageNumber from url
-        		options.pageNumber = options.url.match(/\d/g);
+        		options.pageNumber = options.url.replace(/\D/g, '');
         		options.pageNumber++;
         		// Append pageNumber to url
-        		options.url = options.url.replace(/\d/g, options.pageNumber);
+        		options.url = options.url.replace(/\d+/g, options.pageNumber);
         		// disable button
         		Engine.ui.button.disable($(this));
+
 				$(document).trigger("load.page", options);
 			});
 			options.paginator.prev.on("click", function() {
-				options.pageturner = "prev";
         		// get pageNumber from url
-        		options.pageNumber = options.url.match(/\d/g);
+        		//options.pageNumber = options.url.replace(/\D/g, '');
         		if (options.pageNumber >= 1) {
         			options.pageNumber--;
         			// Append pageNumber to url
-        			options.url = options.url.replace(/\d/g, options.pageNumber);
+        			options.url = options.url.replace(/\d+/g, options.pageNumber);
+        			console.log(options.url);
         			// disable button
         			Engine.ui.button.disable($(this));
 					$(document).trigger("load.page", options);
@@ -82,73 +113,63 @@ jQuery(function ($) {
             		event.preventDefault();
             		// only get page name from url - side1, side2, sidex...
             		options.url = $(this)[0].pathname.split("/").pop().replace(".html", "");
+        			options.pageNumber = options.url.replace(/\D/g, '');
 					$(document).trigger("load.page", options);
             	});
             });
             // Check page status cookie.
             // If cookie stores page and pagenumber then open that page.
-			var pageStatus = Engine.cookie.read('playbook');
+			/*var pageStatus = Engine.cookie.read('playbook');
 			if (pageStatus) {
 				options.url = pageStatus.split(",")[0];
 				options.pageNumber = pageStatus.split(",")[1];
-			}
-			window.setTimeout(function() {
+			}*/
+			/*window.setTimeout(function() {
 				if (pageStatus) {
 					$(".dummy").addClass("animatePrev");
 				}
 				$(document).trigger("load.page", options);
-			}, 900);
+			}, 900);*/
+			//$(document).trigger("load.page", options);
         },
         ui: {
         	showPage: function(options) {
-				$(".site-content__wrapper").hide().removeClass("animateCurrent animatePrev");
-				$('#' + options.previousPage).show().css("z-index", 2).addClass("animatePrev");
-				$('#' + options.url).show().css("z-index", 3).addClass("animateCurrent");
+				
+				// add/remove classes
+				$(".site-content__wrapper").removeClass("animateCurrent animatePrev is-active is-previous");
+				$('#' + options.previousPage).addClass("animatePrev is-previous");
+				$('#' + options.url).addClass("animateCurrent is-active");
+
 				options.previousPage = options.url;
+
 				window.setTimeout(function() {
 					$(".site-content__wrapper").removeClass("animateCurrent animatePrev");
         			Engine.ui.button.enable([ options.paginator.next, options.paginator.prev ]);
 				}, 900);
 			},
-			getPage: function(options) {
+        	addPageToDom: function(pageId, imageId) {
 
-        		if (options.previousPage) {
-					$(".site-content__wrapper").hide().removeClass("animateCurrent animatePrev");
-					$('#' + options.previousPage).show().css("z-index", 2).addClass("animatePrev");
-        		}
+				imageId = imageId * 2;
 
-        		// Get url content and add it to the page
-        		var pageToLoad = options._PAGEDIR_ + options.url + ".html";
-                $.get(pageToLoad)
-                    .done(function(response) {
-						// create new dom element and add new html to it
-						var cache = Engine.ui.addPageToDom(options, response);
-						// and add it to our wrapper
-						cache.addClass("animateCurrent");
-						options.content.prepend(cache);
-						cache = "";
-						// save page state
-						options.previousPage = options.url;
-						window.setTimeout(function() {
-							// remove animate class from pages
-							$(".site-content__wrapper").removeClass("animateCurrent animatePrev");
-        					Engine.ui.button.enable([ options.paginator.next, options.paginator.prev ]);
-        					//Engine.ui.preload(options);
-						}, 1000);
-						Engine.cookie.create('playbook', [ options.url, options.pageNumber ], 2);
-                    })
-					.fail(function() {
-						console.log("last page");
-						// this is the last page, so lets start over
-						options.pageNumber = 0;
-        				// Append pageNumber to url
-        				options.url = options.url.replace(/\d/g, options.pageNumber);
-        				Engine.ui.button.disable(options.paginator.next);
-						$(document).trigger("load.page", options);
-					});
-        	},
-        	addPageToDom: function(options, response) {
-            	return $('<div class="site-content__wrapper" id="' + options.url + '">' + response + '</div>');
+            	var html = '<div class="site-content__wrapper" id="' + pageId + '">';
+					html += '<div class="page">';
+					html += '<div class="col">';
+					html += '<h1>Forside</h1>';
+					html += '<p>Og noget tekst til forsiden</p>';
+					html += '<div class="image-wrap">';
+					html += '<img src="/static/media/images/Playbook_images' + imageId + '.jpg" alt=""/>';
+					html += '</div>';
+					html += '</div>';
+					html += '<div class="col">';
+					html += '<h1>Side 1</h1>';
+					html += '<p>Og noget tekst til side 1</p>';
+					html += '<div class="image-wrap">';
+					html += '<img src="/static/media/images/Playbook_images' + parseInt(imageId + 1) + '.jpg" alt=""/>';
+					html += '</div>';
+					html += '</div>';
+					html += '</div>';
+
+				return html;
         	},
         	button: {
         		disable: function(button) {
@@ -169,29 +190,7 @@ jQuery(function ($) {
         				$(button).removeAttr("disabled");
         			}
         		}
-        	},
-			preload: function(options) {
-        		// get pageNumber from url
-        		options.pageNumber = options.url.match(/\d/g);
-        		options.pageNumber++;
-        		// Append pageNumber to url
-        		options.url = options.url.replace(/\d/g, options.pageNumber);
-        		// Get url content and add it to the page
-        		var pageToLoad = options._PAGEDIR_ + options.url + ".html";
-                $.get(pageToLoad)
-                    .done(function(response) {
-						console.log("page: ", options.url, " preloaded");
-						// create new dom element and add new html to it
-						var cache = Engine.ui.addPageToDom(options, response);
-						// and add it to our wrapper
-						options.content.prepend(cache);
-						cache = "";
-						// save page state
-                    })
-					.fail(function() {
-						console.log("preload failed");
-					});
-			}
+        	}
         },
 		cookie: {
 			create: function (name, value, days) {
@@ -222,8 +221,78 @@ jQuery(function ($) {
     // Initialize main script-Engine;
     Engine.init();
 });
+              /*
+			preload: function(options) {
+        		// get pageNumber from url
+        		options.pageNumber = options.url.match(/\d/g);
+        		options.pageNumber++;
+        		// Append pageNumber to url
+        		options.url = options.url.replace(/\d/g, options.pageNumber);
+        		// Get url content and add it to the page
+        		var pageToLoad = options._PAGEDIR_ + options.url + ".html";
+                $.get(pageToLoad)
+                    .done(function(response) {
+						console.log("page: ", options.url, " preloaded");
+						// create new dom element and add new html to it
+						var cache = Engine.ui.addPageToDom(options, response);
+						// and add it to our wrapper
+						options.content.prepend(cache);
+						cache = "";
+                    })
+					.fail(function() {
+						console.log("preload failed");
+					});
+			} */
+/*			
 
-/*
+                /*if ($('#' + options.url).length) {
+					Engine.ui.showPage(options);
+				} else {
+					Engine.ui.getPage(options);
+				}
+
+			getPage: function(options) {
+
+        		if (options.previousPage) {
+					$(".site-content__wrapper").hide().removeClass("animateCurrent animatePrev");
+					$('#' + options.previousPage).show().css("z-index", 2).addClass("animatePrev");
+        		}
+
+        		// Get url content and add it to the page
+        		var pageToLoad = options._PAGEDIR_ + options.url + ".html";
+                $.get(pageToLoad)
+                    .done(function(response) {
+						// create new dom element and add new html to it
+						var cache = Engine.ui.addPageToDom(options, response);
+						// animate it...
+						cache.addClass("animateCurrent");
+						// ...and add it to our wrapper
+						options.content.prepend(cache);
+						// clear it for next use
+						cache = "";
+						// save page state
+						options.previousPage = options.url;
+						window.setTimeout(function() {
+							// remove animate class from pages
+							$(".site-content__wrapper").removeClass("animateCurrent animatePrev");
+							// enable prev/next buttons again
+        					Engine.ui.button.enable([ options.paginator.next, options.paginator.prev ]);
+        					// preload the next page
+        					Engine.ui.preload(options);
+						}, 1000);
+						Engine.cookie.create('playbook', [ options.url, options.pageNumber ], 2);
+                    })
+					.fail(function() {
+						console.log("last page");
+						// this is the last page, so lets start over
+						options.pageNumber = 0;
+        				// Append pageNumber to url
+        				options.url = options.url.replace(/\d/g, options.pageNumber);
+        				// disable the next button
+        				Engine.ui.button.disable(options.paginator.next);
+						$(document).trigger("load.page", options);
+					});
+        	}
 					options.content.load(pageToLoad, function(response, textStatus, xhr) {
 					 	 //If external page don't exists then roll back the pagenumber
 					 	 //and append that to the url
